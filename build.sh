6432a9b3ca03510678d6fc1a13c89e2eba081350
Gia <opangarry@gmail.com>
2025-12-28 12:26:17 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,20 @@
 #!/system/bin/sh
 
-export UBUNTU_DIR="/cupy/"
-export PATH="$UBUNTU_DIR/usr/bin:$UBUNTU_DIR/usr/sbin:$PATH"
-export LD_LIBRARY_PATH="$UBUNTU_DIR/usr/lib:$UBUNTU_DIR/lib"
-export HOME="/gia/"
-export TMPDIR="/tmp"
 
+export APP_DATA="/data/data/onegiaterm.ewirs.web.id/files/gia"
+export UBUNTU_DIR="$APP_DATA/cupy"
+
+
+export PATH="$UBUNTU_DIR/usr/bin:$UBUNTU_DIR/usr/sbin:$PATH"
+
+export LD_LIBRARY_PATH="$UBUNTU_DIR/usr/lib:$UBUNTU_DIR/lib:$UBUNTU_DIR/usr/lib/arm-linux-gnueabihf"
+
+
+export HOME="$APP_DATA/home"
+export TMPDIR="$APP_DATA/tmp"
+
+if [ ! -d "$HOME" ]; then mkdir -p "$HOME"; fi
+if [ ! -d "$TMPDIR" ]; then mkdir -p "$TMPDIR"; fi
+
+cd $HOME
+exec /system/bin/sh -i
