ed7c5c9a0403fdd6c2606ac0e59fd4ddca159f12
Gia <opangarry@gmail.com>
2025-12-28 12:39:18 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 
 
-export APP_DATA="/data/data/onegiaterm.ewirs.web.id/files/gia"
+export APP_DATA="/data/user/0/onegiaterm.ewirs.web.id/files/gia"
 export UBUNTU_DIR="$APP_DATA/cupy"
 
 
