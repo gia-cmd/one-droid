936d3cb0bdc27b4d00cb8d30b86ca385eae2a9ce
Gia <opangarry@gmail.com>
2025-12-28 12:12:22 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 export UBUNTU_DIR="/cupy/"
 export PATH="$UBUNTU_DIR/usr/bin:$UBUNTU_DIR/usr/sbin:$PATH"
 export LD_LIBRARY_PATH="$UBUNTU_DIR/usr/lib:$UBUNTU_DIR/lib"
-export HOME="/cupy/root"
+export HOME="/gia/"
 export TMPDIR="/tmp"
 
