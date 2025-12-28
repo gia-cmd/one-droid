a4d1aab08a075b8415e79826d71333d8b552a42c
Gia <opangarry@gmail.com>
2025-12-28 17:27:56 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,25 @@
 #!/system/bin/sh
 
-
-export APP_DATA="/data/user/0/id.web.ewirs.gia/files/gia"
+# Jalur absolut yang benar sesuai package name Anda
+export APP_DATA="/data/user/0/id.web.ewirs.gia/files"
 export UBUNTU_DIR="$APP_DATA/ubuntu"
+export HOME="$APP_DATA/gia"
+export LINKER="$UBUNTU_DIR/lib/ld-linux-armhf.so.3"
 
-
+# Setup Paths
 export PATH="$UBUNTU_DIR/usr/bin:$UBUNTU_DIR/usr/sbin:$PATH"
-
 export LD_LIBRARY_PATH="$UBUNTU_DIR/usr/lib:$UBUNTU_DIR/lib:$UBUNTU_DIR/usr/lib/arm-linux-gnueabihf"
 
-
-export HOME="$APP_DATA/gia"
-export TMPDIR="$APP_DATA/tmp"
-
-if [ ! -d "$HOME" ]; then mkdir -p "$HOME"; fi
-if [ ! -d "$TMPDIR" ]; then mkdir -p "$TMPDIR"; fi
+# Buat folder jika belum ada
+[ ! -d "$HOME" ] && mkdir -p "$HOME"
 
 cd $HOME
-see /system/bin/sh -i
+
+# TANPA PROOT: Kita harus memanggil Bash Ubuntu melalui Linker-nya
+# Cek apakah Bash Ubuntu tersedia
+if [ -f "$UBUNTU_DIR/bin/bash" ]; then
+    exec $LINKER $UBUNTU_DIR/bin/bash --login
+else
+    # Fallback ke sh Android jika bash belum ada
+    exec /system/bin/sh -i
+fi
