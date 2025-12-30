572ded7d95643e2f123d2feddfdc0572e66779ae
Gia <opangarry@gmail.com>
2025-12-30 21:53:03 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/system/bin/sh
-vicum https://cdimage.ubuntu.com/ubuntu-base/releases/jammy/release/https://www.php.net/distributions/php-8.4.16.tar.gz && gia --tar xf php-8.4.16.tar.gz && rename ozi php
+echo "Halo dari Android shell script!"
+echo "Waktu sekarang: $(date)"
+echo "Saya berjalan di: $(pwd)"
